840fe495fe034673a084fed04580310c7bf26285
Andrew Moorman <andrewmoormanmskcc@gmail.com>
2024-04-11 14:48:13 -0400

    refactored

--- modules/sift/bin/slide_image.py
+++ modules/sift/bin/slide_image.py
@@ -49,6 +49,7 @@ class SlideImage(ABC):
     file_path : typing.Union[str, os.PathLike]
         Path of the file to load
     """
+
     def __init__(
         self,
         file_path: typing.Union[str, os.PathLike],
@@ -56,39 +57,25 @@ class SlideImage(ABC):
         # Set file path for lazy reading
         self._file_path = file_path
         
-        # '_shapes' and '_levels' are helper params to enable more convenient
-        # indexing of pyramidal files through a 'img.levels[level]' pattern
-        # of indexing
-        self._set_shapes()
-        self._levels = []
-        for i in self._shapes.keys():
-            self._levels.append(Level(slide_image=self, level=i))
-
+        # '_levels' is a helper param to enable more convenient indexing of 
+        # pyramidal files through a 'img.levels[level]' pattern
+        self._set_levels()
 
     @abstractmethod
-    def _set_shapes(self) -> typing.Dict[str, OrderedDict]:
+    def _set_levels(self) -> list:
         """
-        Creates a 'shapes' nested dictionary mapping level indices to ordered
-        dictionaries where:
-        keys :
-            Axes ordered by their dimensional ordering in the image.
-        values : 
-            Their sizes, e.g., number of pixels for X and Y or number of images
-            for Z. This should the same or similar to ndarray.shape.
-
-        Developer notes: Although the shape metadata can often be found in the
-        file metadata sections, we copy this info to a new dictionary to avoid
-        frequently re-opening the file, e.g., when doing slice validation. 
+        Creates a list of 'levels' whose indices are the order of pyramidal
+        levels, where 0 is the largest or bottom level.
         
         Returns
         -------
-        typing.Dict[str, OrderedDict]
-            A dictionary of axis shapes as described above
+        list
+            A list of Level objects
         """
         pass
 
     @abstractmethod
-    def _get_slice(self, level, **kwargs) -> np.ndarray:
+    def _get_slice(self, level : int, **kwargs) -> np.ndarray:
         """
         Helper function to enable array slicing/indexing aritrary dimension
         orderings.
@@ -104,6 +91,31 @@ class SlideImage(ABC):
         np.ndarray
             sliced Numpy array described by kwargs
         """
+        return self._get_slice_child(level, **kwargs)
+
+    @abstractmethod
+    def _get_level_dtype(self, level: int) -> np.dtype:
+        """
+        Helper function for data type by pyramidal level.
+        Returns e.g., dtype('uint16') for a 16-bit integer array
+        """
+        pass
+
+    @abstractmethod
+    def _get_level_axes(self, level: int) -> str:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        pass
+
+    @abstractmethod
+    def _get_level_shape(self, level: int) -> tuple[int]:
+        """
+        Helper function for getting dimensional sizes by pyramidal level.
+        Returns e.g., (1000, 1000, 10, 3) for 4d array with shape 
+        1000 x 1000 x 10 x 3.
+        """
         pass
 
     def __getitem__(self, key):
@@ -113,29 +125,6 @@ class SlideImage(ABC):
         By default, slices the 0th pyramidal level.
         """
         return self._levels[0][key]
-
-    def _get_level_order(self, level: int) -> str:
-        """
-        Helper function for getting the ordering of axes by pyramidal level.
-        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
-        """
-        return ''.join(self._shapes[level].keys())
-
-    def _get_level_shape(self, level: int) -> tuple[int]:
-        """
-        Helper function for getting dimensional sizes by pyramidal level.
-        Returns e.g., (1000, 1000, 10, 3) for 4d array with shape 
-        1000 x 1000 x 10 x 3.
-        """
-        return tuple(self._shapes[level].values())
-
-    @abstractmethod
-    def _get_level_dtype(self, level: int) -> np.dtype:
-        """
-        Helper function for data type by pyramidal level.
-        Returns e.g., dtype('uint16') for a 16-bit integer array
-        """
-        pass
 
     def select_level(
         self,
@@ -170,25 +159,24 @@ class SlideImage(ABC):
         ValueError
             If no level meets the requirements
         """
-        # Convert max memory argument from string (e.g., '10GB') to int
-        units = {"B": 1, "KB": 10**3, "MB": 10**6, "GB": 10**9, "TB": 10**12}
-        def parse_size(size):
+        # Internal helper
+        def parse_size(size : str):
+            units = {"B": 1, "KB": 1e3, "MB": 1e6, "GB": 1e9, "TB": 1e12}
             size = size.upper()
             if not re.match(r' ', size):
                 size = re.sub(r'([KMGT]?B)', r' \1', size)
-            number, unit = [string.strip() for string in size.split()]
-            return int(float(number)*units[unit])
+            val, unit = map(str.strip, size.split())
+            return int(float(val) * units[unit])
+
         if isinstance(max_mem, str):
-            max_bytes = parse_size(max_mem)
-        
-        # Get level with at most n_bytes
+            max_mem = parse_size(max_mem)
         for level in self.levels:
             if by_dims is None:
                 by_dims = level.axes
             mask = [d in list(by_dims) for d in level.axes]
             shape = np.array(level.shape)[mask]
             n_bytes = np.prod(shape) * level.dtype.itemsize
-            if n_bytes <= max_bytes:
+            if n_bytes <= max_mem:
                 return level
         msg = (
             f"No level found matching memory request of {max_mem} for "
@@ -199,17 +187,40 @@ class SlideImage(ABC):
     @property
     def levels(self):
         """
-        Dictionary-like object with keys indexing into the pyramidal levels of 
-        the file, returning 'Level' convenience objects.
+        List indexing into the pyramidal levels of the file, returning 'Level'
+        convenience objects.
         """
         return self._levels
 
     @property
-    def mpp(self):
+    def axes(self) -> str:
         """
-        Microns per pixel of each axis
+        Ordering of axes, e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        By default, references the 0th pyramidal level.
         """
-        pass
+        return self._levels[0].axes
+
+    @property
+    def shape(self) -> tuple[int]:
+        """
+        Dimensional sizes, e.g., (1000, 1000, 10, 3) for 4d array with shape 
+        1000 x 1000 x 10 x 3.
+        By default, references the 0th pyramidal level.
+        """
+        return self._levels[0].shape
+    
+    # Decorator to check level values across all
+    @staticmethod
+    def _check_level(fn):
+        def wrapper(self, *args, **kwargs):
+            named_args = dict(zip(fn.__code__.co_varnames[1:], args))
+            named_args.update(kwargs)
+            level = named_args['level']
+            if len(self._levels) <= level:
+                msg = f"User-provided level '{level}' does not exist"
+                raise ValueError(msg)
+            return fn(self, *args, **kwargs)
+        return wrapper
 
 
 @_register_image_format(['.tif', '.tiff'])
@@ -222,13 +233,12 @@ class TIFFSlideImage(SlideImage):
     file_path : str
         Path to a valid Tiff file
     """
-    def __init__(self, file_path):
-        super().__init__(file_path)
+    _check_level = SlideImage._check_level
 
-    def _set_shapes(self):
+    def _set_levels(self):
         """
-        Creates a 'shapes' nested dictionary mapping level indices to ordered
-        dictionaries.
+        Creates a list of 'levels' objects corresponding to pyramidal levels in
+        Tiff
 
         Raises
         ------
@@ -243,18 +253,101 @@ class TIFFSlideImage(SlideImage):
                     "than one series or no series"
                 )
                 raise NotImplementedError(msg)
-            elif len(f.series) == 1:
-                shapes = dict()
-                for i, level in enumerate(f.series[0].levels):
-                    # channels axis is named S in some Tiffs
-                    axes = level.get_axes().replace('S', 'C')
-                    shapes[i] = OrderedDict(zip(
-                        list(axes),
-                        level.get_shape()
-                    ))
+            else:
+                self._levels = [
+                    Level(slide_image=self, level=i)
+                    for i in range(len(f.series[0].levels))
+                ]
 
-            self._shapes = shapes
+    @_check_level
+    def _get_slice(
+        self,
+        level: int = 0,
+        **kwargs,
+    ) -> np.ndarray:
+        """
+        Helper function to enable array slicing/indexing in Tiff files with
+        arbitrary dimension orderings.
 
+        Parameters
+        ----------
+        level : int, optional
+            Pyramidal level to access, by default 0
+        kwargs : dict
+            A dictionary of axis names (keys) and slice objects (values)
+        Returns
+        -------
+        np.ndarray
+            sliced Numpy array described by index
+        """
+        index = defaultdict(lambda: slice(None))
+        index.update(kwargs)
+        #assert self._validate_slice(index)
+        store = tifffile.imread(
+            self._file_path,
+            aszarr=True,
+            level=level,
+        )
+        z = zarr.open(store)
+        slices = tuple(index[ax] for ax in self._get_level_axes(level))
+        return z[slices]
+
+    @_check_level
+    def _get_level_dtype(self, level: int) -> np.dtype:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        with tifffile.TiffFile(self._file_path, mode='r') as f:
+            return f.series[0].levels[level].dtype
+
+    @_check_level
+    def _get_level_axes(self, level: int) -> str:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        with tifffile.TiffFile(self._file_path, mode='r') as f:
+            return f.series[0].levels[level].axes
+
+    @_check_level
+    def _get_level_shape(self, level: int) -> str:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        print(level)
+        with tifffile.TiffFile(self._file_path, mode='r') as f:
+            return f.series[0].levels[level].shape
+
+'''
+# Concrete creator for .nd2 files
+# TODO: No good solution for working with .nd2 files which is memory-efficient
+# and fast, so loading full image to memory for now
+@_register_image_format('.nd2')
+class ND2SlideImage(SlideImage):
+    """
+    Default creator for nd2 (Nikon NIS Elements) file format
+
+    Parameters
+    ----------
+    file_path : str
+        Path to a valid nd2 file
+    """
+
+    _check_level = SlideImage._check_level
+
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self._img = nd2.imread(self._file_path)
+
+    def _set_levels(self):
+        """
+        ND2 files can only contain one level
+        """
+        self._levels = [Level(slide_image=self, level=0)]
+
+    @_check_level
     def _get_slice(
         self,
         level: int = 0,
@@ -287,24 +380,37 @@ class TIFFSlideImage(SlideImage):
         slices = tuple(index[ax] for ax in self._get_level_order(level))
         return z[slices]
 
+    @_check_level
     def _get_level_dtype(self, level: int) -> np.dtype:
         """
-        Helper function for data type by pyramidal level.
-        Returns e.g., dtype('uint16') for a 16-bit integer array
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
         """
         with tifffile.TiffFile(self._file_path, mode='r') as f:
             return f.series[0].levels[level].dtype
 
-'''
-# Concrete creator for .nd2 files
-# TODO: No good solution for working with .nd2 files which is memory-efficient
-# and fast, so loading full image to memory for now
-@_register_image_format('.nd2')
-class ND2SlideImage(SlideImage):
+    @_check_level
+    def _get_level_axes(self, level: int) -> str:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        with tifffile.TiffFile(self._file_path, mode='r') as f:
+            return f.series[0].levels[level].dims
+
+    @_check_level
+    def _get_level_shape(self, level: int) -> str:
+        """
+        Helper function for getting the ordering of axes by pyramidal level.
+        Returns e.g., 'XYZC' for 4d array with X, Y, Z, C axes.
+        """
+        with tifffile.TiffFile(self._file_path, mode='r') as f:
+            return f.series[0].levels[level].shape
+
     def __init__(self, file_path):
         super().__init__(file_path)
         # Initialize nd2 object as ndarray
-        self._img = nd2.imread(self._file_path)
+        
         # Set length, layer size(s), etc.
         self._set_metadata()
 
@@ -320,8 +426,8 @@ class ND2SlideImage(SlideImage):
     def __getitem__(self, key):
         # Implementation for slicing .nd2 images
         return self._img[key]
-
-
+'''
+'''
 # TODO: Concrete creator for .mrxs files
 class MRXSSlideImage(SlideImage):
     def __init__(self, file_path):
@@ -366,7 +472,7 @@ class Level:
         """
         Getter for parent property
         """
-        return self.slide_image._get_level_order(self.level)
+        return self.slide_image._get_level_axes(self.level)
     
     @property
     def shape(self) -> tuple[int]:
@@ -388,7 +494,7 @@ class Level:
         """
         try:
             iter(item)
-        except TypeError as te:
+        except TypeError:
             item = [item]
         index = dict(zip(list(self.axes), item))
         return self.slide_image._get_slice(self.level, **index)
--- modules/sift/bin/test_slide_image.py
+++ modules/sift/bin/test_slide_image.py
@@ -18,14 +18,11 @@ tif_img.levels
 path_to_tif = '../../../resources/HE.tif'
 tif_img = slide_image.get_slide_image(path_to_tif)
 tif_img._get_slice(
+    level=0,
     X=slice(None, 10),
     Y=slice(None, 10),
     S=0
 )
-
-tif_img.
-
-
 
 
 
@@ -47,39 +44,41 @@ f.metadata
 {'k': 'v'}.pop
 
 
-def get_args(fn):
-    print(fn.__code__.co_varnames)
-    def wrapper(*args, **kwargs):
-        kwargs.update({'world': 'moon'})
-        fn(*args, **kwargs)
-    return wrapper
+from PIL import Image
+from PIL.TiffTags import TAGS
 
-def hello_world(
-    hello,
-    world
-):
-    print(hello, world)
+with Image.open(path_to_tif) as img:
+    for key in img.tag.keys():
+        try:
+            print(TAGS[key], img.tag[key])
+        except:
+            pass
+    #meta_dict = {TAGS[key] : img.tag[key] for key in img.tag.keys()}
 
-get_args(hello_world)(hello='hello', world='world')
 
-from functools import partial
-partial(hello_world, sun='sun')(world='world')
+img = Image.open(path_to_tif)
 
-def test(hello:int):
-    pass
+for key in img.tag.keys():
+    print(key)
 
-test('hi')
-
-tif_img = slide_image.get_slide_image(path_to_tif)
-tif_img[0]
+img.tag[key]
 
 import tifffile
 import zarr
 
 tifffile.TiffFile(path_to_tif).pages[0].tags['ImageWidth'].value
 tifffile.TiffFile(path_to_tif).pages[0].tags['ImageLength'].value
-tifffile.TiffFile(path_to_tif).series[1]
-tifffile.TiffFile(path_to_tif).series[0].levels[0].nbytes
+for attribute in filter(
+    lambda x: '_metadata' in x,
+    dir(tifffile.TiffFile(path_to_tif))
+):
+    print(getattr(tifffile.TiffFile(path_to_tif), attribute))
+
+
+tifffile.TiffFile(path_to_tif).andor_metadata
+
+
+tifffile.TiffFile(path_to_tif).series[0].levels[0]
 
 tifffile.TiffFile()
 
@@ -91,8 +90,30 @@ store = tifffile.imread(
     path_to_tif,
     aszarr=True,
     level=1,
+)
 
 z = zarr.open(store)
 
 import numpy as np
 np.dtype('uint16').itemsize
+
+
+    
+def validate_hello(fn):
+    def validate(*args, **kwargs):
+        level = dict(zip(fn.__code__.co_varnames, args))['level']
+        if len(self._levels) < level:
+            raise ValueError(f"Supplied level {level} not found in file")
+        return fn(*args, **kwargs)
+    return validate
+
+@validate_hello
+def hello_world(
+    hello,
+    world,
+):
+    pass
+
+hello_world(1,2)
+
+pass
